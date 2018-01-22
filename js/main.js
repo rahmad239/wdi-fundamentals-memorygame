b99046794e51c849624d9205ce43d434f65a6410
Qya <raqayyah@msn.com>
2018-01-22 17:54:13 -0500

    created game logic

--- js/main.js
+++ js/main.js
@@ -1,7 +1,19 @@
+var cards = ["queen", "queen", "king", "king"];
+var cardsInPlay = [];
+var cardOne = cards[0];
+var cardTwo = cards [2];
 
-var cardOne = "queen";
-var cardTwo = "queen";
-var cardThree = "king";
-var cardFour= "king";
-console.log("User Flipped" + cardOne);
-console.log("User Flipped" + cardTwo);
+cardsInPlay.push('cardOne');
+console.log("User Flipped queen");
+cardsInPlay.push('cardTwo');
+console.log("User Flipped king");
+
+if (cardsInPlay.length === 2) {
+	result = "checkForMatch";
+} 
+ if (cardsInPlay[0] === cardsInPlay[1]) { 
+	alert ("You Found A Match!"); 
+} else { 
+	alert ("Sorry try again");
+}
+
